d52222188fdeaebc40ca3b563a8f83bcc98cf5b7
cmldk <cemal.dak@gmail.com>
2025-06-09 14:21:57 +0300

    base changes

--- src/consts.ts
+++ src/consts.ts
@@ -1,44 +1,42 @@
-import type { Site, Metadata, Socials } from "@types";
+import type { Site, Metadata, Socials } from '@types';
 
 export const SITE: Site = {
-  NAME: "Astro Nano",
-  EMAIL: "markhorn.dev@gmail.com",
+  NAME: 'Cemal Dak',
+  HEADER_NAME: 'cmldk',
+  EMAIL: 'cemal.dak@gmail.com',
   NUM_POSTS_ON_HOMEPAGE: 3,
   NUM_WORKS_ON_HOMEPAGE: 2,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
-  TITLE: "Home",
-  DESCRIPTION: "Astro Nano is a minimal and lightweight blog and portfolio.",
+  TITLE: 'Home',
+  DESCRIPTION: 'Personal blog and portfolio.',
 };
 
 export const BLOG: Metadata = {
-  TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+  TITLE: 'Blog',
+  DESCRIPTION: 'A collection of articles on topics I am passionate about.',
 };
 
 export const WORK: Metadata = {
-  TITLE: "Work",
-  DESCRIPTION: "Where I have worked and what I have done.",
+  TITLE: 'Work',
+  DESCRIPTION: 'Where I have worked and what I have done.',
 };
 
 export const PROJECTS: Metadata = {
-  TITLE: "Projects",
-  DESCRIPTION: "A collection of my projects, with links to repositories and demos.",
+  TITLE: 'Projects',
+  DESCRIPTION:
+    'A collection of my projects, with links to repositories and demos.',
 };
 
 export const SOCIALS: Socials = [
-  { 
-    NAME: "twitter-x",
-    HREF: "https://twitter.com/markhorn_dev",
+  {
+    NAME: 'github',
+    HREF: 'https://github.com/cmldk',
   },
-  { 
-    NAME: "github",
-    HREF: "https://github.com/markhorn-dev"
+  {
+    NAME: 'linkedin',
+    HREF: 'https://www.linkedin.com/in/cemaldak',
   },
-  { 
-    NAME: "linkedin",
-    HREF: "https://www.linkedin.com/in/markhorn-dev",
-  }
 ];
