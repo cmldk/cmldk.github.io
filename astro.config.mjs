74502628b9901ac6faaa7efd26b30842d59c1765
cmldk <cemal.dak@gmail.com>
2025-07-30 20:32:12 +0300

    config updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,4 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   site: 'https://cmldk.github.io',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: 'hybrid',
 });
